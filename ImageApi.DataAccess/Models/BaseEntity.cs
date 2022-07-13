4eca88c32fdb7a44f36c54eaad31167cefc42eb2
Baek <kontakt@baek.pro>
2022-07-13 14:23:01 +0200

    Add project files.

---
+++ ImageApi.DataAccess/Models/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace ImageApi.DataAccess.Models
+{
+    /// <summary>
+    /// Base class for all database models
+    /// </summary>
+    public abstract class BaseEntity
+    {
+        /// <summary>
+        /// Primary id
+        /// </summary>
+        public Guid Id { get; set; }
+    }
+}
---
+++ ImageApi.DataAccess/Models/Primary/Account/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace ImageApi.DataAccess.Models.Primary.Account
+{
+    public class Account : BaseEntity
+    { 
+        public bool Enabled { get; set; }
+
+        public string Email { get; set; }    
+    }
+}
