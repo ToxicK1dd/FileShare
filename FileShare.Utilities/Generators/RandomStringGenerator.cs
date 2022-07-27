3ed6b4989c7a7cb588895d84da97363dfe5b9239
Baek <kontakt@baek.pro>
2022-07-28 01:15:37 +0200

    Added comments to helpers

--- FileShare.Utilities/Generators/RandomStringGenerator.cs
+++ FileShare.Utilities/Generators/RandomStringGenerator.cs
@@ -2,8 +2,16 @@
 
 namespace FileShare.Utilities.Generators
 {
+    /// <summary>
+    /// Static helper methods for generating strings.
+    /// </summary>
     public static class RandomStringGenerator
     {
+        /// <summary>
+        /// Generate random base64 string.
+        /// </summary>
+        /// <param name="length">The length of the generated string. Defaults to 64.</param>
+        /// <returns>A base64 encoded random string of characters.</returns>
         public static string Generate(int length = 64)
         {
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#¤%&/()=?~^*-_<>\\";
--- FileShare.Utilities/Helpers/IdentityClaimsHelper.cs
+++ FileShare.Utilities/Helpers/IdentityClaimsHelper.cs
@@ -3,8 +3,16 @@ using System.Security.Claims;
 
 namespace FileShare.Utilities.Helpers
 {
+    /// <summary>
+    /// Static helper methods for claims.
+    /// </summary>
     public static class IdentityClaimsHelper
     {
+        /// <summary>
+        /// Static helper method to get account id from HTTP context.
+        /// </summary>
+        /// <param name="httpContext"></param>
+        /// <returns>The id of the account if found. Otherwise Guid.Empty.</returns>
         public static Guid GetAccountIdFromHttpContext(this HttpContext httpContext)
         {
             if (httpContext.User.Identity.IsAuthenticated is false)
