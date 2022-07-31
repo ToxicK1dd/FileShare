0bf68bbc68c53c1339f95f2f58353fbbbb988fef
Baek <kontakt@baek.pro>
2022-07-31 19:05:05 +0200

    Added minified filter case plugin

--- FileShare.Api/wwwroot/swagger-ui/custom.js
+++ FileShare.Api/wwwroot/swagger-ui/custom.js
@@ -1,1 +1,15 @@
-﻿
+﻿window.addEventListener("load", () => {
+    let c = function () {
+        return {
+            fn: {
+                opsFilter: (a, b) => a.filter((_, a) => -1 !== a.toLowerCase().indexOf(b.toLowerCase()))
+            }
+        }
+    }, b = window.SwaggerUIBundle;
+    function a(a) {
+        return a.presets.push(c),
+            b(...arguments)
+    }
+    a.__proto__ = b,
+        window.SwaggerUIBundle = a
+});
